0748ab11b98bdbc7ee9e18f01927b4d99535baf6
BasantPandeya <59715795+BasantPandeya@users.noreply.github.com>
2020-01-18 18:42:17 +0545

    update

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,41 @@
-users = {
-    'name': 'Ram',
-    'address': 'KTM',
-    'phone': 9843363777,
-}
-print(users['name'])
-print(users['address'])
-print(users['phone'])
+# users = {
+#     'name': 'Ram',
+#     'address': 'KTM',
+#     'phone': 9843363777,
+# }
+# print(users['name'])
+# print(users['address'])
+# print(users['phone'])
+
+# users=[
+#     {
+#         'name':'ram',
+#         'address':'ktm',
+#         'phone': 98062,
+#         'parent':[
+#             'hari'
+#         ]
+#     }
+# ]
+# print(users[0]['parent'][0]) '''users ko 0 index vitra parent basyo, ani parent ko 0 index vitra hari'''
+
+# users={
+#         'name':'ram',
+#         'address':'ktm',
+#         'phone': 98062,
+#         'parent':[
+#             'hari'
+#         ]
+#     }
+#
+# users['address']='Lalitpur'
+# print (users)
+
+students=[
+    {'name':'hari', 'address':98449},
+    {'name':'sita', 'address':'ktm'},
+    {'name':'gita', 'address':'ktm'}
+]
+for student in students:
+    print (student['name'], student['address'])
+
