6ac346de3aa2de97a1c137e60125f7ebea4a39f4
BasantPandeya <59715795+BasantPandeya@users.noreply.github.com>
2020-01-20 08:41:38 +0545

    update

--- decorator.py
+++ decorator.py
@@ -1,12 +1,34 @@
-def get_doc(anyfunctionhere):
-    def innherfunction():
-        return anyfunctionhere.__doc__
+# def get_doc(anyfunctionhere):
+#     def innerfunction():
+#         return anyfunctionhere.__doc__
+#
+#     return innerfunction
+#
+# @get_doc
+# def getname(name):
+#     """i am user's name"""
+#     return "all name"
+#
+# print(getname())
 
-    return innherfunction
+def age_check(anyfunction):
+    def ack(age):
+        if age>=18 and age<=40:
+            return "Welcome to party"
+        else:
+            return "We are sorry. We cannot allow you to enter"
+    return ack
 
-@get_doc
-def getname(name):
-    """i am user's name"""
-    return "all name"
+@age_check
+def getage(age):
+    return age
 
-print(getname())
+a=int(input('Enter your age: '))
+print(getage(a))
+
+# zero check decorator
+# @check user name is string or not
+# @display documentation
+# functionwrap
+# def user(name)
+#     """something to write"""
